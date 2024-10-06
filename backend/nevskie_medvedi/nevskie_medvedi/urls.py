2d613c297e465af636da0c75145a442ae883f192
snyderlt <103443107+snyderlt@users.noreply.github.com>
2024-10-07 02:32:03 +0800

    Update urls.py

--- backend/nevskie_medvedi/nevskie_medvedi/urls.py
+++ backend/nevskie_medvedi/nevskie_medvedi/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework.routers import DefaultRouter
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from coach.views import CoachViewSet, EventViewSet, SessionViewSet
 from client.views import ClientViewSet, ChildViewSet
 
@@ -11,7 +14,24 @@ router.register(r'events', EventViewSet)
 router.register(r'clients', ClientViewSet)
 router.register(r'children', ChildViewSet)
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Project API",
+      default_version='v1',
+      description="API documentation for all available endpoints",
+      terms_of_service="https://www.yourcompany.com/policies/terms/",
+      contact=openapi.Contact(email="contact@yourcompany.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    # Swagger URLs
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
