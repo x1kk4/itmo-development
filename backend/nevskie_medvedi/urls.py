ea1f118ded5d41eff49341a88f6c6f2aa2f321c0
x1kk4 <grigory.valevin@gmail.com>
2024-10-11 00:14:43 +0300

    fixed api/v1/ url

--- backend/nevskie_medvedi/urls.py
+++ backend/nevskie_medvedi/urls.py
@@ -29,7 +29,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
    path('admin/', admin.site.urls),
-   path('api/v1', include(router.urls)),
+   path('api/v1/', include(router.urls)),
 
    # Swagger URLs
    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
