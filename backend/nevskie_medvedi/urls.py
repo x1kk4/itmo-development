6b073e1587c9ddd4d03950c6fe5de773e84afecd
x1kk4 <grigory.valevin@gmail.com>
2024-10-10 23:32:48 +0300

    updated configs

--- backend/nevskie_medvedi/urls.py
+++ backend/nevskie_medvedi/urls.py
@@ -29,10 +29,13 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/', include(router.urls)),
-    # Swagger URLs
+   path('admin/', admin.site.urls),
+   path('api/v1/', include(router.urls)),
+   # Swagger URLs
    #  path('api/v1/swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('api/v1/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('api/v1/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
    #  path('api/v1/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+if settings.DEBUG:
+   urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
