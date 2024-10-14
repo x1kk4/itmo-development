1753c3c04449242a05612a3e3400d933387da0a6
x1kk4 <grigory.valevin@gmail.com>
2024-10-14 19:28:52 +0300

    added empty calendar

--- web/src/views/Schedule/index.tsx
+++ web/src/views/Schedule/index.tsx
@@ -1,10 +1,29 @@
 import { FC } from 'react'
+import { Calendar, dayjsLocalizer } from 'react-big-calendar'
+import dayjs from 'dayjs'
+import { Box } from '@chakra-ui/react'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+
+const localizer = dayjsLocalizer(dayjs)
 
 const Schedule: FC = () => {
   return (
-    <>
-      <p>Schedule</p>
-    </>
+    <Box
+      height={'calc(100% - 50px)'}
+      padding={4}
+      border={'1px'}
+      borderColor={'gray.300'}
+      borderRadius={'lg'}
+    >
+      <Calendar
+        localizer={localizer}
+        defaultDate={new Date()}
+        startAccessor='start'
+        endAccessor='end'
+        style={{ maxHeight: '840px' }}
+        defaultView='week'
+      />
+    </Box>
   )
 }
 
