d8ed69a063d55bc3c3fddceb961d10dfc535d421
x1kk4 <grigory.valevin@gmail.com>
2024-10-14 11:29:57 +0300

    populations call

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,6 +2,8 @@
 
 # Run migrations
 python3 manage.py migrate
+python3 manage.py populate_coaches
+python3 manage.py populate_clients
 
 # Start the application
 exec gunicorn nevskie_medvedi.wsgi:application --bind 0.0.0.0:8000
