bfa5a2dae0963822dfa3aea4d5329d633108c317
x1kk4 <grigory.valevin@gmail.com>
2024-10-14 09:00:53 +0300

    fixed a;ksldjf;aklsjdhf;lk

--- backend/nevskie_medvedi/urls.py
+++ backend/nevskie_medvedi/urls.py
@@ -38,4 +38,4 @@ urlpatterns = [
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('api/v1/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
