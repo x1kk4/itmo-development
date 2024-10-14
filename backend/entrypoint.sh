b8ce6851b438579392363dfb0b7cec199a14049a
x1kk4 <grigory.valevin@gmail.com>
2024-10-14 17:39:51 +0300

    fixed entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # Run migrations
-python3 manage.py migrate populate_db
+python3 manage.py migrate
+python3 manage.py populate_db
 
 # Start the application
 exec gunicorn nevskie_medvedi.wsgi:application --bind 0.0.0.0:8000
