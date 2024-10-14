ea16c59dc0f41e4ca14f686e917860f45ad73a89
x1kk4 <grigory.valevin@gmail.com>
2024-10-14 10:48:05 +0300

    fixed deploy )

--- backend/nevskie_medvedi/settings.py
+++ backend/nevskie_medvedi/settings.py
@@ -85,13 +85,15 @@ WSGI_APPLICATION = "nevskie_medvedi.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME", "postgres"),
+        'USER': os.getenv("DB_USER", "postgres"),
+        'PASSWORD': os.getenv("DB_PASSWORD", "postgres"),
+        'HOST': os.getenv("DB_HOST", "db"),
+        'PORT': os.getenv("DB_PORT", "5432")
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
