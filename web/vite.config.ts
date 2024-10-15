8ed0698c734d98c1d01cb32b3eaf8760113030e6
x1kk4 <grigory.valevin@gmail.com>
2024-10-15 18:18:41 +0300

    fixed vite config

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import checker from 'vite-plugin-checker'
-import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,7 +15,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': resolve('/src'),
+      '@': '/src',
     },
   },
 })
