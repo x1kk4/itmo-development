438d6e9eb1dd7375820c48e8cde83bb1538f34e3
x1kk4 <grigory.valevin@gmail.com>
2024-10-15 18:42:09 +0300

    test

--- web/src/router/pageRoutes.tsx
+++ web/src/router/pageRoutes.tsx
@@ -6,10 +6,10 @@ import { RouteProps } from 'react-router-dom'
 import { LandingPage } from '@/pages/landing/index'
 import { SignInPage } from '@/pages/auth/sign-in/index'
 import { SignUpPage } from '@/pages/auth/sign-up/index'
-import { DashboardPage } from '@/pages/dashboard/index'
-import { SchedulePage } from '@/pages/schedule/index'
-import { BranchesPage } from '@/pages/branches/index'
-import { SubscriptionPage } from '@/pages/subscription/index'
+// import { DashboardPage } from '@/pages/dashboard/index'
+// import { SchedulePage } from '@/pages/schedule/index'
+// import { BranchesPage } from '@/pages/branches/index'
+// import { SubscriptionPage } from '@/pages/subscription/index'
 
 type TPageRoute = RouteProps & {
   allowedRoles: ROLE[]
@@ -35,30 +35,30 @@ const pageRoutes: TPageRoute[] = [
     allowedRoles: [ROLE.UNAUTHORIZED],
     layout: LAYOUT.AUTH,
   },
-  {
-    path: routes.dashboard,
-    element: <DashboardPage />,
-    allowedRoles: [ROLE.PARENT],
-    layout: LAYOUT.PARENT,
-  },
-  {
-    path: routes.schedule,
-    element: <SchedulePage />,
-    allowedRoles: [ROLE.PARENT],
-    layout: LAYOUT.PARENT,
-  },
-  {
-    path: routes.branches,
-    element: <BranchesPage />,
-    allowedRoles: [ROLE.PARENT],
-    layout: LAYOUT.PARENT,
-  },
-  {
-    path: routes.subscription,
-    element: <SubscriptionPage />,
-    allowedRoles: [ROLE.PARENT],
-    layout: LAYOUT.PARENT,
-  },
+  // {
+  //   path: routes.dashboard,
+  //   element: <DashboardPage />,
+  //   allowedRoles: [ROLE.PARENT],
+  //   layout: LAYOUT.PARENT,
+  // },
+  // {
+  //   path: routes.schedule,
+  //   element: <SchedulePage />,
+  //   allowedRoles: [ROLE.PARENT],
+  //   layout: LAYOUT.PARENT,
+  // },
+  // {
+  //   path: routes.branches,
+  //   element: <BranchesPage />,
+  //   allowedRoles: [ROLE.PARENT],
+  //   layout: LAYOUT.PARENT,
+  // },
+  // {
+  //   path: routes.subscription,
+  //   element: <SubscriptionPage />,
+  //   allowedRoles: [ROLE.PARENT],
+  //   layout: LAYOUT.PARENT,
+  // },
 ]
 
 export { pageRoutes }
