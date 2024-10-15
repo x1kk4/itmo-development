2b23459f9fcbb63f4475af37c6bfca501c374dbb
x1kk4 <grigory.valevin@gmail.com>
2024-10-15 18:19:44 +0300

    test

--- web/src/router/pageRoutes.tsx
+++ web/src/router/pageRoutes.tsx
@@ -2,13 +2,13 @@ import { ROLE } from './types'
 import { routes } from './routes'
 import { RouteProps } from 'react-router-dom'
 
-import { LandingPage } from '@/pages/landing'
+// import { LandingPage } from '@/pages/landing'
 import { SignInPage } from '@/pages/auth/sign-in'
 import { SignUpPage } from '@/pages/auth/sign-up'
-import { DashboardPage } from '@/pages/dashboard'
-import { SchedulePage } from '@/pages/schedule'
-import { BranchesPage } from '@/pages/branches'
-import { SubscriptionPage } from '@/pages/subscription'
+// import { DashboardPage } from '@/pages/dashboard'
+// import { SchedulePage } from '@/pages/schedule'
+// import { BranchesPage } from '@/pages/branches'
+// import { SubscriptionPage } from '@/pages/subscription'
 import { LAYOUT } from '@/layouts'
 
 type TPageRoute = RouteProps & {
@@ -17,12 +17,12 @@ type TPageRoute = RouteProps & {
 }
 
 const pageRoutes: TPageRoute[] = [
-  {
-    path: routes.landing,
-    element: <LandingPage />,
-    allowedRoles: [ROLE.UNAUTHORIZED, ROLE.PARENT, ROLE.COACH, ROLE.MANAGER],
-    layout: LAYOUT.LANDING,
-  },
+  // {
+  //   path: routes.landing,
+  //   element: <LandingPage />,
+  //   allowedRoles: [ROLE.UNAUTHORIZED, ROLE.PARENT, ROLE.COACH, ROLE.MANAGER],
+  //   layout: LAYOUT.LANDING,
+  // },
   {
     path: routes['sign-in'],
     element: <SignInPage />,
@@ -35,29 +35,29 @@ const pageRoutes: TPageRoute[] = [
     allowedRoles: [ROLE.UNAUTHORIZED],
     layout: LAYOUT.AUTH,
   },
-  {
-    path: routes.dashboard,
-    element: <DashboardPage />,
-    allowedRoles: [ROLE.PARENT],
-    layout: LAYOUT.PARENT,
-  },
-  {
-    path: routes.schedule,
-    element: <SchedulePage />,
-    allowedRoles: [ROLE.PARENT],
-    layout: LAYOUT.PARENT,
-  },
-  {
-    path: routes.branches,
-    element: <BranchesPage />,
-    allowedRoles: [ROLE.PARENT],
-    layout: LAYOUT.PARENT,
-  },
-  {
-    path: routes.subscription,
-    element: <SubscriptionPage />,
-    allowedRoles: [ROLE.PARENT],
-    layout: LAYOUT.PARENT,
+  // {
+  //   path: routes.dashboard,
+  //   element: <DashboardPage />,
+  //   allowedRoles: [ROLE.PARENT],
+  //   layout: LAYOUT.PARENT,
+  // },
+  // {
+  //   path: routes.schedule,
+  //   element: <SchedulePage />,
+  //   allowedRoles: [ROLE.PARENT],
+  //   layout: LAYOUT.PARENT,
+  // },
+  // {
+  //   path: routes.branches,
+  //   element: <BranchesPage />,
+  //   allowedRoles: [ROLE.PARENT],
+  //   layout: LAYOUT.PARENT,
+  // },
+  // {
+  //   path: routes.subscription,
+  //   element: <SubscriptionPage />,
+  //   allowedRoles: [ROLE.PARENT],
+  //   layout: LAYOUT.PARENT,
   },
 ]
 
