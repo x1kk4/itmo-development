3bee32122ad08e201c2fb2cd70cb6ff8d06e50c7
x1kk4 <grigory.valevin@gmail.com>
2024-10-16 13:32:38 +0300

    temp

--- web/src/views/Schedule/index.tsx
+++ web/src/views/Schedule/index.tsx
@@ -1,15 +1,41 @@
-import { FC } from 'react'
-import { Calendar, dayjsLocalizer } from 'react-big-calendar'
+import { FC, useMemo, useState } from 'react'
+import { Calendar, dayjsLocalizer, type Formats, type Event } from 'react-big-calendar'
 import dayjs from 'dayjs'
-import { Box } from '@chakra-ui/react'
+import { Box, useDisclosure } from '@chakra-ui/react'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
+import styles from './schedule.module.css'
+import { events } from './events'
+import { AppointmentModal } from './AppointmentModal'
 
 const localizer = dayjsLocalizer(dayjs)
 
 const Schedule: FC = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const [selectedEvent, setSelectedEvent] = useState<Event | null>(null)
+
+  const formats: Formats = useMemo(
+    () => ({
+      dateFormat: 'ddd',
+
+      dayFormat: (date) => localizer.format(date, 'DD ddd', 'ru'),
+
+      // dayRangeHeaderFormat: ({ start, end }, culture, localizer) =>
+      //   localizer.format(start, { date: 'short' }, culture) +
+      //   ' — ' +
+      //   localizer.format(end, { date: 'short' }, culture),
+    }),
+    [],
+  )
+
+  const handleEventClick = (event: Event) => {
+    setSelectedEvent(event)
+    onOpen()
+  }
+
   return (
     <Box
       height={'calc(100% - 50px)'}
+      maxH={'calc(100% - 50px)'}
       padding={4}
       border={'1px'}
       borderColor={'gray.300'}
@@ -20,8 +46,18 @@ const Schedule: FC = () => {
         defaultDate={new Date()}
         startAccessor='start'
         endAccessor='end'
-        style={{ maxHeight: '840px' }}
         defaultView='week'
+        className={styles.calendar}
+        views={['week', 'month']}
+        events={events}
+        onSelectEvent={handleEventClick}
+        step={20}
+        formats={formats}
+      />
+      <AppointmentModal
+        isOpen={isOpen}
+        onClose={onClose}
+        event={selectedEvent}
       />
     </Box>
   )
