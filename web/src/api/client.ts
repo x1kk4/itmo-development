ee6c9cf6d05bd18fae43666e172173a36340ce51
x1kk4 <grigory.valevin@gmail.com>
2024-10-17 21:52:13 +0300

    no creds

--- web/src/api/client.ts
+++ web/src/api/client.ts
@@ -4,8 +4,8 @@ export const api = axios.create({
   timeout: 10000,
   baseURL: 'https://itmo.website/api/v1',
   headers: {
-    'Access-Control-Allow-Credentials': true,
+    // 'Access-Control-Allow-Credentials': true,
     'Content-Type': 'application/json',
   },
-  withCredentials: true,
+  // withCredentials: true,
 })
