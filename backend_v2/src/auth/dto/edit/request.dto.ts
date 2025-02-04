250b7ca05c9bed455d7ad75ee1840f56c37cc799
x1kk4 <grigory.valevin@gmail.com>
2025-02-04 05:10:07 +0300

    optionals )

--- backend_v2/src/auth/dto/edit/request.dto.ts
+++ backend_v2/src/auth/dto/edit/request.dto.ts
@@ -1,23 +1,28 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsPhoneNumber, MaxLength } from 'class-validator';
+import { IsOptional, IsPhoneNumber, MaxLength } from 'class-validator';
 
 export class EditProfileRequestDto {
   @ApiProperty({ example: '+79219211337', required: false })
+  @IsOptional()
   @IsPhoneNumber()
   phone: string | null;
 
   @ApiProperty({ example: '@gregjs', required: false })
+  @IsOptional()
   telegram: string | null;
 
   @ApiProperty({ example: 'Валерий', required: false })
+  @IsOptional()
   @MaxLength(32)
   firstname: string | null;
 
   @ApiProperty({ example: 'Жмышенко', required: false })
+  @IsOptional()
   @MaxLength(32)
   surname: string | null;
 
   @ApiProperty({ example: 'Альбертович', required: false })
+  @IsOptional()
   @MaxLength(32)
   middlename: string | null;
 }
